f7cbe4409a455fbdc707fe00a83e876b9af30cb6
chika3742 <kazu.chika.shima@gmail.com>
2023-06-22 16:38:21 +0900

    feat: playgroundにi18nを導入

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,24 @@
 export default defineNuxtConfig({
-  modules: ["../src/module"],
+  modules: ["../src/module", "@nuxtjs/i18n"],
   mhyMaterialComponents: {},
   // @ts-ignore
   build: {
     transpile: ["vuetify"],
   },
   devtools: {enabled: true},
+  i18n: {
+    locales: [
+      {
+        code: "ja",
+        iso: "ja-JP",
+        file: "ja.yaml",
+      },
+    ],
+    langDir: "./locales/",
+    defaultLocale: "ja",
+    vueI18n: {
+      legacy: false,
+      fallbackLocale: "ja",
+    },
+  },
 })
