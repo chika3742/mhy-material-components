acce0d4fb3af94479e1a06b3725d495889875e16
chika3742 <kazu.chika.shima@gmail.com>
2023-06-22 15:23:57 +0900

    feat: Playgroundの基本UI

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,46 @@
-<template>
-  <div>
-    Nuxt module playground!
-    <PityCounterPage />
-  </div>
-</template>
+<script setup lang="ts">
+import {ref} from "#imports"
 
-<script setup>
+const isDrawerOpen = ref(false)
+
+interface DrawerItem {
+  title: string
+  to: string
+}
+
+const drawerItems: DrawerItem[] = [
+  {title: "Home", to: "/"},
+  {title: "Pity Counter", to: "/pity-counter"},
+]
 </script>
+
+<template>
+  <v-app>
+    <v-app-bar title="Component Playground">
+      <template #prepend>
+        <v-app-bar-nav-icon
+          icon="mdi-menu"
+          @click="isDrawerOpen = !isDrawerOpen"
+        />
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-item
+          v-for="item in drawerItems"
+          :key="item.title"
+          :to="item.to"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <NuxtPage />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
