b253a1f0f50e50637bd91ad5d7129bf84ab228b6
chika3742 <kazu.chika.shima@gmail.com>
2023-07-17 18:07:46 +0900

    fix: なぜかSASSが読み込めないため、SCSSに変更

--- src/runtime/components/character/card.vue
+++ src/runtime/components/character/card.vue
@@ -34,34 +34,37 @@ defineProps<{
   </v-card>
 </template>
 
-<style lang="sass" scoped>
-.character-card
-  aspect-ratio: 2 / 1
-  max-width: 250px
-  width: calc(50% - 16px)
+<style lang="scss" scoped>
+.character-card {
+  aspect-ratio: 2 / 1;
+  max-width: 250px;
+  width: calc(50% - 16px);
 
-  &__element
-    position: absolute
-    right: 12%
-    top: 20%
-    bottom: 20%
-    aspect-ratio: 1
-    opacity: 0.7
-    background-size: cover
-    filter: brightness(0.5)
+  &__element {
+    position: absolute;
+    right: 12%;
+    top: 20%;
+    bottom: 20%;
+    aspect-ratio: 1;
+    opacity: 0.7;
+    background-size: cover;
+    filter: brightness(0.5);
+  }
 
-  span
-    position: absolute
-    bottom: 0
-    left: 0
-    padding: 4px 16px
-    background: linear-gradient(to right, rgba(0, 0, 0, 0.6) 70%, rgba(0, 0, 0, 0))
-    font-size: 1.3rem
-    color: white
-    text-shadow: 0 0 4px black
+  span {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 4px 16px;
+    background: linear-gradient(to right, rgba(0, 0, 0, 0.6) 70%, rgba(0, 0, 0, 0));
+    font-size: 1.3rem;
+    color: white;
+    text-shadow: 0 0 4px black;
+  }
 
-    @media (max-width: 450px)
-      font-size: 1em
-      padding: 2px 8px
-
+  @media (max-width: 450px) {
+    font-size: 1em;
+    padding: 2px 8px;
+  }
+}
 </style>
