f994c49151fcb2d78987f76a8cc0b2b5e0b2a443
chika3742 <kazu.chika.shima@gmail.com>
2023-11-01 19:33:50 +0900

    fix: sass to scss

--- src/runtime/components/page/release-notes.vue
+++ src/runtime/components/page/release-notes.vue
@@ -115,6 +115,51 @@ const timelineStyle = computed(() => {
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+.changelog-title {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 
+  &--mobile {
+    display: flex;
+    flex-direction: column;
+    padding: 4px 16px;
+    margin-bottom: 8px;
+    background: rgb(var(--v-theme-card));
+    border-radius: 8px;
+    width: 250px;
+    max-width: 250px;
+  }
+}
+
+.timeline {
+  overflow: hidden;
+  padding: 0 16px;
+  transition: height 300ms ease;
+}
+
+.show-more-blur {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+
+  &__btn {
+    z-index: 2;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    backdrop-filter: blur(4px);
+    mask: linear-gradient(to bottom, transparent 0%, black 50%);
+  }
+}
 </style>
