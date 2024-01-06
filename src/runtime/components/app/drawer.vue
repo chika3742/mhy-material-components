bd192848fb2b22e216edb6ebbacb812646198d5e
chika3742 <kazu.chika.shima@gmail.com>
2024-01-06 17:47:59 +0900

    fix: ドロワーのパディングを修正

--- src/runtime/components/app/drawer.vue
+++ src/runtime/components/app/drawer.vue
@@ -28,28 +28,27 @@ const isOpen = computed({
   <v-navigation-drawer
     v-model="isOpen"
   >
-    <v-list
-      v-safe-area="{left: 8, top: true}"
-      nav
-    >
-      <template v-for="(item, i) in drawerItems">
-        <v-list-item
-          v-if="item !== '---'"
-          :key="i"
-          :href="item.href"
-          :prepend-icon="item.icon"
-          :target="item.target"
-          :title="item.title ? $t(`navDrawer.${item.title}`) : $getPageTitle(item.to!)"
-          :to="item.to ? localePath(item.to) : undefined"
-          density="comfortable"
-          @click="item.onClick?.(); isOpen = false"
-        />
-        <v-divider
-          v-else
-          :key="i * 2"
-          class="mb-1"
-        />
-      </template>
-    </v-list>
+    <div v-safe-area="{top: true, left: true}">
+      <v-list nav>
+        <template v-for="(item, i) in drawerItems">
+          <v-list-item
+            v-if="item !== '---'"
+            :key="i"
+            :href="item.href"
+            :prepend-icon="item.icon"
+            :target="item.target"
+            :title="item.title ? $t(`navDrawer.${item.title}`) : $getPageTitle(item.to!)"
+            :to="item.to ? localePath(item.to) : undefined"
+            density="comfortable"
+            @click="item.onClick?.(); isOpen = false"
+          />
+          <v-divider
+            v-else
+            :key="i * 2"
+            class="mb-1"
+          />
+        </template>
+      </v-list>
+    </div>
   </v-navigation-drawer>
 </template>
